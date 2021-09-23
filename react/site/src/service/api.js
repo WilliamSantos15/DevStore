8088de493692e332ca8f5e42be5d8c95c8c30a54
William <williamsantos041015@gmail.com>
2021-09-23 11:26:35 -0300

    AAAAAAAAAAAAAA

--- react/site/src/service/api.js
+++ react/site/src/service/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const api = axios.create({
-    baseURL: 'https://devstorefrei.herokuapp.com/'
+    baseURL: "https://devstorefrei.herokuapp.com"
 })
 
 export default class Api {
